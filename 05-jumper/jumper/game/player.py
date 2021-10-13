5cba32f591566ade3f908e4dc1b15b8304582d62
Ellwood12 <han16059@byui.edu>
2021-10-13 15:43:16 -0600

    Here is some work i've been doing

--- 05-jumper/jumper/game/player.py
+++ 05-jumper/jumper/game/player.py
@@ -1,5 +1,21 @@
+import numpy as np
+
 class Player:
-   def __init__(self):
-       pass
-   def get_guess(self):
-      pass
+    def __init__(self):
+        pass
+
+    def ask_letter():
+        guess = input("Guess a letter [a-z]: ")
+        return guess
+    
+    def guess_letter():
+        """
+        Add the letter to the library
+        """
+        lib = []
+        letter = (Player.ask_letter())
+        while letter in lib:
+            print("That letter was already chosen:")
+            letter = (Player.ask_letter())
+        lib.append(letter)
+        return lib
