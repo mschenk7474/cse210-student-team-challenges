ea462219bf765175af9eab1fdc461d9054a811eb
mikeyhampy <90983524+mikeyhampy@users.noreply.github.com>
2021-10-14 13:42:49 -0600

    parachute man

--- 05-jumper/jumper/game/parachute_man.py
+++ 05-jumper/jumper/game/parachute_man.py
@@ -1,6 +1,11 @@
 class Parachute_man:
    def __init__(self):
-       pass
+      self.parachute_beg = "  ___\n /___\ \n \   /\n  \ /\n   0\n  /|\ \n  / \ \n\n^^^^^^^"
+      self.parachute1 = " /___\ \n \   /\n  \ /\n   0\n  /|\ \n  / \ \n\n^^^^^^^"
+      self.parachute2 = " \   /\n  \ /\n   0\n  /|\ \n  / \ \n\n^^^^^^^"
+      self.parachute3 = "  \ /\n   0\n  /|\ \n  / \ \n\n^^^^^^^"
+      self.parachute_dead = "   X\n  /|\ \n  / \ \n\n^^^^^^^"
+
    def display(self):
       pass
    def variation_of_p_man(self):
