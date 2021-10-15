a9c1cc9c32c81f21745b7fadf4aae16b077de377
magoodsell <goo15004@byui.edu>
2021-10-15 11:23:35 -0600

    Finished class game_master.

--- 05-jumper/jumper/game/game_master.py
+++ 05-jumper/jumper/game/game_master.py
@@ -1,5 +1,43 @@
+import re
+from game.word_bank import Word_Bank
+from game.player import Player
+
 class Game_Master:
-   def __init__(self):
-       pass
-   def check_guess(self):
-      pass
+
+    def __init__(self):
+        """
+        An instance of the class Game_master 
+
+        Attributes: 
+              player: an instance of the class Player()
+              guess (boolean): keeps track if the player guess correctly or not
+        """
+
+        self.player = Player()
+        self.word_bank = Word_Bank()
+        self.guess_check = False
+        self.letters_right = 0
+        
+
+    def check_guess(self):
+        """
+        Makes sure the guess of players guess is correct. 
+        """
+        # get the players letter guess 
+        player_guess = self.player.ask_letter()
+        # make the guess a lowercase 
+        player_guess.lower()
+        # save the word as a variable 
+        word = self.word_bank.get_word()
+        
+        # count the number of times the letter is in the word
+        self.letters_right = word.count(player_guess)
+
+        
+        if self.letters_right > 0: 
+          guess_check = True
+        else: 
+          guess_check = False
+
+        return guess_check
+
