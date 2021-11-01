bbb381292bccc29cccc01a69cf43dd09b0f0e1b0
Mschenk <masonschenk@gmail.com>
2021-11-01 10:46:59 -0600

    Here is the input service

--- 07-speed/speed/game/input_service.py
+++ 07-speed/speed/game/input_service.py
@@ -1,46 +1,4 @@
 import sys
-
-class InputService:
-    """Detects player input. The responsibility of the class of objects is to detect player keypresses and translate them into a point representing a direction (or velocity).
-
-    Stereotype: 
-        Service Provider
-
-    Attributes:
-        _screen (Screen): An Asciimatics screen.
-    """
-
-    def __init__(self, screen):
-        """The class constructor.
-        
-        Args:
-            self (InputService): An instance of InputService.
-        """
-        self._screen = screen
-        
-    def get_letter(self):
-        """Gets the letter that was typed. If the enter key was pressed returns an asterisk.
-
-        Args:
-            self (InputService): An instance of InputService.
-
-        Returns:
-            string: The letter that was typed.
-        """
-        result = ""
-        event = self._screen.get_key()
-        if not event is None:
-            if event == 27:
-                sys.exit()
-            elif event == 10: 
-                result = "*"
-            elif event >= 97 and event <= 122: 
-                result = chr(event)
-        return result
-
-#Snake Game 
-
-import sys
 from game.point import Point
 import raylibpy
 
@@ -114,3 +72,11 @@ class InputService:
         Determines if the user is trying to close the window
         """
         return raylibpy.window_should_close()
+
+    def get_letter(self):
+        key_int = raylibpy.get_key_pressed()
+ 
+        key_string = None
+        if key_int != -1:
+            key_string = chr(key_int)
+        return key_string
