18b114c0a951359da4bb448cc75383d0ab404b11
mikeyhampy <90983524+mikeyhampy@users.noreply.github.com>
2021-11-03 11:18:29 -0600

    updated buffer

--- 07-speed/speed/game/buffer.py
+++ 07-speed/speed/game/buffer.py
@@ -15,8 +15,16 @@ class Buffer():
    def get_string(self):
       return self._string
    def match(self, word_list):
-      for x in word_list:
-         if self._string == word_list[x]:
-            return 1
-         else:
-            return 0
+      is_in_word = 0
+      number_loop = len(word_list)
+      x = 0
+      while(is_in_word == 0 and x <= number_loop):
+
+         is_in_word = self._string.count(word_list[x])
+         x += 1
+
+      if is_in_word > 0:
+         return True
+      else:
+         return False
+   #if returns true, the word that's correct is word_list[x-1]
